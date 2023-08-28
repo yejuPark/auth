78e8371043a98854c42cd4c80d6d5f144ff757be
yejuPark <pyj8977@daum.net>
2023-08-28 15:09:47 +0900

    logout

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm, CustomAuthenticationForm
 from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 
 # Create your views here.
 
@@ -34,3 +35,9 @@ def login(request):
         'form': form,
     }
     return render(request, 'login.html', context)
+
+
+
+def logout(request):
+    auth_logout(request)
+    return redirect('accounts:login')
